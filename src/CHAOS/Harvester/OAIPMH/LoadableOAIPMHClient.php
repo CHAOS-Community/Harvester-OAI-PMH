74492e601d886a98ebd89d3182900518f1836947
Kræn Hansen <post@creen.dk>
2013-01-17 19:40:51 +0100

    Created a loadable extension of the OAIPMHClient

---
+++ src/CHAOS/Harvester/OAIPMH/LoadableOAIPMHClient.php
@@ -0,0 +1,29 @@
+<?php
+namespace CHAOS\Harvester\OAIPMH;
+class LoadableOAIPMHClient extends \oaipmh\OAIPMHClient implements \CHAOS\Harvester\IExternalClient {
+	/**
+	 * A reference to the harvester.
+	 * @var \CHAOS\Harvester\ChaosHarvester
+	 */
+	protected $_harvester;
+	
+	/**
+	 * Constructs a new DFIClient for communication with the Danish Film Institute open API.
+	 * @param string $baseURL
+	 */
+	public function __construct($harvester, $name, $parameters = array()) {
+		parent::__construct($parameters['baseURL'], $harvester->hasOption('debug'));
+		$this->_harvester = $harvester;
+		$this->_harvester->debug("Created a client for OAI-PMH endpoint: %s", $this->_baseURL);
+	}
+	
+	public function debug($message) {
+		$this->_harvester->debug($message);
+	}
+	
+	public function sanityCheck() {
+		$response = parent::Identify();
+		$this->_harvester->info("Connected to the OAI-PMH endpoint named '%s' administrated by %s.", $response->Identify->repositoryName, $response->Identify->adminEmail);
+		return true;
+	}
+}
