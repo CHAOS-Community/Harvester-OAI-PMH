613815d50a6a6914742f1a245cb42bb94c53e0d2
Kræn Hansen <post@creen.dk>
2013-01-17 19:40:04 +0100

    Filter deleted OAI records, this should almost always be used.

---
+++ src/CHAOS/Harvester/OAIPMH/Filters/RecordDeletedFilter.php
@@ -0,0 +1,15 @@
+<?php
+namespace CHAOS\Harvester\OAIPMH\Filters;
+class RecordDeletedFilter extends \CHAOS\Harvester\Filters\Filter {
+	
+	
+	public function passes($externalObject) {
+		/* @var $externalObject \SimpleXMLElement */
+		
+		if(strval($externalObject->header->attributes()->status) === "deleted") {
+			return "The record was deleted.";
+		} else {
+			return true;
+		}
+	}
+}
